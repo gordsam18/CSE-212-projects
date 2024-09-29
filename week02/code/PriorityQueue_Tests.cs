ac1ee9297908465551daa39fc5fe8267fa407171
samgo23 <gordsam18@gmail.com>
2024-09-28 21:28:03 -0600

    w02 code priority queue

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -9,23 +9,27 @@ public class PriorityQueueTests
     [TestMethod]
     // Scenario: Create a queue of people with varying priorites bob p2, Tim p5, Sue p3
     // Expected Result:  Tim
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Defect(s) Found: Failure in dequeuing loop 
+    public void TestPriorityQueue_CorrectPriority()
     {
+        // set item variables 
         var bob = new PriorityItem("Bob", 2);
         var tim = new PriorityItem("Tim", 5);
         var sue = new PriorityItem("Sue", 3);
-
+        
+        // sets expected result
         PriorityItem expectedResult = tim;
 
-
+        // Enqueue people with priorites
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue(bob.Value, bob.Priority);
         priorityQueue.Enqueue(tim.Value, tim.Priority);
         priorityQueue.Enqueue(sue.Value, sue.Priority);
 
+        // dequeue person with highest priority 
         var highestPriorityPerson = priorityQueue.Dequeue();
 
+        // Throws failure if incorrect person is dequeued
         if (expectedResult.Value != highestPriorityPerson)
         {
             Assert.Fail("Did not return the correct priority. ");
@@ -37,29 +41,64 @@ public class PriorityQueueTests
     // Scenario: Tests to see if the first highest priority item is taken from the queue when there are more than one item with the same priortiy 
     // Expected Result: tim 
     // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void TestPriorityQueue_RepeatPriority()
     {
+        // Set priority items 
         var bob = new PriorityItem("Bob", 2);
         var tim = new PriorityItem("Tim", 5);
         var john = new PriorityItem("John", 4);
         var sue = new PriorityItem("Sue", 5);
 
+        // set expected result 
         PriorityItem expectedResult = tim;
 
-
+        // Enqueue values and priority
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue(bob.Value, bob.Priority);
         priorityQueue.Enqueue(tim.Value, tim.Priority);
         priorityQueue.Enqueue(john.Value, john.Priority);
         priorityQueue.Enqueue(sue.Value, sue.Priority);
-
+        
+        // set first dequeued item 
         var highestPriorityPerson = priorityQueue.Dequeue();
 
+        // throw expection to handle failure 
         if (expectedResult.Value != highestPriorityPerson)
         {
             Assert.Fail("Did not return the correct priority. ");
         }
         Assert.AreEqual(expectedResult.Value, highestPriorityPerson);
     }
+
+    [TestMethod]
+    // Scenario: Tests to see if the first highest priority item is taken from the queue when there are more than one item with the same priortiy 
+    // Expected Result: tim 
+    // Defect(s) Found: 
+    public void TestPriorityQueue_EmptyQueue()
+    {
+        var item = new PriorityQueue();
+
+        try
+        {
+            item.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
+        }
+    }
 }
+
 
