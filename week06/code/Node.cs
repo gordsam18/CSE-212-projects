0eaeebd158ff8b90081fd3f93344638d7d23aa3b
samgo23 <gordsam18@gmail.com>
2024-10-26 14:46:05 -0600

    W06 1-4

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,34 +12,74 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
-        if (value < Data)
+        if ( value == Data)
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
+            return;
         }
         else
         {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Right.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if ( value == Data)
+        {
+            return true;
+        }
+        else
+        {
+            if (value < Data)
+            {
+                // Contains to the left
+                if (Left != null)
+                   return Left.Contains(value);
+                else
+                    return false;
+            }
+            else
+            {
+                // Contains to the right
+                if (Right != null)
+                    return Right.Contains(value);
+                else
+                    return false;
+            }
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        
+
+        if (Right is null && Left is null)
+        {
+            return 1;
+        }
+        else
+        {
+            int rightSide = Right?.GetHeight() ?? 0;
+            int leftSide = Left?.GetHeight() ?? 0;
+
+            return 1 + int.Max(leftSide, rightSide);
+        }
+        
     }
 }
